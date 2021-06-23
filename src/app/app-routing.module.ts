161cd222a284c48fd69add7874400289fca6dafa
gautamb97 <www.gautam971997@gmail.com>
2021-06-23 17:13:02 +0530

    refactor: route for registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : RegistrationComponent },
+  { path: 'register', component : RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
