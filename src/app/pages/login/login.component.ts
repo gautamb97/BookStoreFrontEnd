ebf94bfe5919cb2832fbbfd0e041d748bb3afd75
gautamb97 <www.gautam971997@gmail.com>
2021-06-23 19:37:13 +0530

    refactor: API integration for login of BookStore

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from './login.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar,
+              private service: UserService) { }
 
   ngOnInit(): void {
   }
 
+  user = new Login();
+  message: any
+  data: any
+  error: any
+  token: any
+  userData: any
+
+  submit() {
+    this.service.login(this.user).subscribe(response => {
+      console.log(response)
+      this.data = response
+      this.message = this.data.message
+      this.token = this.data.token
+      localStorage.setItem('token', this.token)
+      this.snackBar.open(this.message, '', { duration: 2000 })
+    }, error => {
+      console.log(error)
+      this.error = error
+      this.message = this.error.error.error
+      this.snackBar.open(this.message, '', { duration: 2000 })
+    })
+  }
 }
