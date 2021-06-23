2f9176090d4f75423eba5102735eebf01de0d780
gautamb97 <www.gautam971997@gmail.com>
2021-06-23 18:40:58 +0530

    refactor: error message if any field is empty

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { User } from './sign-up.model';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -11,5 +14,35 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value'
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  firstName = new FormControl('', Validators.required);
+  getError() {
+    return (this.firstName.hasError('required')) ? 'You must enter a value' : 'Must conatin three letters and only alphabets allowed';
+  }
+
+  lastName = new FormControl('', Validators.required);
+  errorMessage() {
+    return (this.lastName.hasError('required')) ? 'You must enter a value' : 'Must conatin three letters and only alphabets allowed';
+  }
+
+  password = new FormControl('', Validators.required);
+  passwordErrorMessage() {
+    return (this.password.hasError('required')) ? 'You must enter a value' : 'Must have One Capital, one small latter and one number and a symbol';
+  }
+
+
+  user = new User();
+  message: any
+  data: any
+  error: any
 
 }
