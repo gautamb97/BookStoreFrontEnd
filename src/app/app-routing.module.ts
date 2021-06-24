37cbca5586278463ae5603813e5f54b0a4d20f9b
gautamb97 <www.gautam971997@gmail.com>
2021-06-24 20:07:16 +0530

    refactor: route for reset and forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: '', component : RegistrationComponent },
-  { path: 'register', component : RegistrationComponent }
+  { path: 'register', component : RegistrationComponent }, 
+  { path: 'forgotPassword', component: ForgotPasswordComponent},
+  { path: 'resetPassword/:token', component: ResetPasswordComponent}
 ];
 
 @NgModule({
