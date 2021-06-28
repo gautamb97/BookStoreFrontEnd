a5d8de925bbbe8a34a6193c756159fef0767b26a
gautamb97 <www.gautam971997@gmail.com>
2021-06-28 09:36:46 +0530

    refactor: route for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: '', component : RegistrationComponent },
   { path: 'register', component : RegistrationComponent }, 
   { path: 'forgotPassword', component: ForgotPasswordComponent},
-  { path: 'resetPassword/:token', component: ResetPasswordComponent}
+  { path: 'resetPassword/:token', component: ResetPasswordComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
